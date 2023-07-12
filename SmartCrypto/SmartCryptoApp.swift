59fe9e8126fffc1a5792b7a7e062334a0e16e568
Abhishek <Choudhary>
2023-07-12 17:35:43 +0530

    [ADD]: assets folder to project

---
+++ SmartCrypto/SmartCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCryptoApp.swift
+//  SmartCrypto
+//
+//  Created by abhishek on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
