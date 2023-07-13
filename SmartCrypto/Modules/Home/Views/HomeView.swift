cd908e59aafe08afc9d8d1d431709ef8598c44ab
Abhishek <Choudhary>
2023-07-13 12:13:53 +0530

    [ADD] : animations and toggle showPortfolio state variable on button tapped

--- SmartCrypto/Modules/Home/Views/HomeView.swift
+++ SmartCrypto/Modules/Home/Views/HomeView.swift
@@ -8,27 +8,53 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    //MARK: - Properties
+    @State private var showPortfolio : Bool = false
+    
+    //MARK: - View Builder
     var body: some View {
         ZStack {
             Color.theme.backgroundColor
                 .ignoresSafeArea()
             
             VStack{
-                Text("Header")
+                HStack(content: {
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(.none)
+                        .background(
+                          CircleButtonAnimation(animate: $showPortfolio)
+                        )
+                    Spacer()
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .animation(.none)
+                        .foregroundColor(Color.theme.accentColor)
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .animation(.none)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()){
+                                showPortfolio.toggle()
+                            }
+                        }
+                }).padding(.horizontal)
+                
                 Spacer(minLength: 0)
             }
         }
-
     }
 }
 
+//MARK: - Previews
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView(content: {
             HomeView()
                 .navigationBarHidden(true)
         })
-        
     }
 }
 
