3c482fe96a3ee06afa948bf94fce84f5a755d370
Abhishek <Choudhary>
2023-07-19 19:00:07 +0530

    [UPDATE] : Over ride navigation bar title appearence in apps entry point

--- SmartCrypto/SmartCryptoApp.swift
+++ SmartCrypto/SmartCryptoApp.swift
@@ -12,6 +12,11 @@ struct SmartCryptoApp: App {
     
     @StateObject private var homeViewModel = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accentColor)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accentColor)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView{
